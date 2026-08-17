75414f302c394a907ebb33626056cb487d53b9ef
ruwansameera606-afk <ruwansameera606@gmail.com>
2026-08-17 14:14:33 +0530

    Create NavGraph.kt

---
+++ app/src/main/res/values/app/src/main/java/com/example/familyremotesupport/NavGraph.kt
@@ -0,0 +1,9 @@
+package com.example.familyremotesupport
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NavGraph() {
+    Text("Family Remote Support")
+}
